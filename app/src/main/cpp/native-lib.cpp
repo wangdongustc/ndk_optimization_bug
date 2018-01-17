e298c0fed589cfcbb63e47d775a6cf429810732a
Darren Dong Wang <wangdong115@foxmail.com>
2018-01-17 19:55:01 +0800

    reduce two cases

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,17 +5,6 @@
 #include <string.h>
 #include <android/log.h>
 
-#define PACKAGE_NAME "com.polytpetech.libusb_bug"
-
-#ifndef USE_CUSTOM_LOG
-#define USE_CUSTOM_LOG
-#define TAG PACKAGE_NAME
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
-#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, TAG, __VA_ARGS__)
-#endif
-
 // the entrance function
 int tmain();
 
@@ -32,7 +21,7 @@ Java_com_polytopetech_libusb_1bug_MainActivity_stringFromJNI(
 }
 
 
-/** A copy of the libusb function usbi_parse_descriptor
+/** A modified copy of the libusb function usbi_parse_descriptor
  *  https://github.com/libusb/libusb/blob/84209b4fb54ebc62f5a60f0122bddef3ebd97d0a/libusb/descriptor.c#L45
  *
  *
@@ -56,31 +45,6 @@ int usbi_parse_descriptor(const unsigned char *source, const char *descriptor,
       case 'b':	/* 8-bit byte */
         *dp++ = *sp++;
         break;
-      case 'w':	/* 16-bit word, convert from little endian to CPU */
-        dp += ((uintptr_t)dp & 1);	/* Align to word boundary */
-
-        if (host_endian) {
-          memcpy(dp, sp, 2);
-        } else {
-          w = (sp[1] << 8) | sp[0];
-          *((uint16_t *)dp) = w;
-        }
-        sp += 2;
-        dp += 2;
-        break;
-      case 'd':	/* 32-bit word, convert from little endian to CPU */
-        dp += ((uintptr_t)dp & 1);	/* Align to word boundary */
-
-        if (host_endian) {
-          memcpy(dp, sp, 4);
-        } else {
-          d = (sp[3] << 24) | (sp[2] << 16) |
-              (sp[1] << 8) | sp[0];
-          *((uint32_t *)dp) = d;
-        }
-        sp += 4;
-        dp += 4;
-        break;
       case 'u':	/* 16 byte UUID */
         memcpy(dp, sp, 16);
         sp += 16;
@@ -93,8 +57,8 @@ int usbi_parse_descriptor(const unsigned char *source, const char *descriptor,
 }
 
 struct Struct {
-  uint8_t a;
-  uint8_t b;
+  unsigned char a;
+  unsigned char b;
 };
 
 int tmain() {
